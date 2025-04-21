e3e2cc5b75c50a9f7498553d218f53467751ce1d
Hassan <kkromah12@gmail.com>
2025-04-22 01:18:43 +0300

    edited

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -64,7 +64,7 @@ const ProjectList = () => {
   return (
     <>
       <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5">
-        <section className="filterSection">
+        <section className="filterSection md:order-1 order-2">
           <Card className="p-5 sticky top-10">
             <div className="flex justify-between lg:w-[20rem]">
               <p className="text-xl -tracking-wider">filters</p>
